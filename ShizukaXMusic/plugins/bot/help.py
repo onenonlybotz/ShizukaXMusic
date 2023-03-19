5955d73b6159595ce7381b7ff375597a03b78c1c
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-03-19 12:13:54 +0530

    Update help.py

--- ShizukaXMusic/plugins/bot/help.py
+++ ShizukaXMusic/plugins/bot/help.py
@@ -69,7 +69,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb5":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "ᴏɴʟʏ ғᴏʀ ᴏᴡɴᴇʀ ᴀɴᴅ sᴜᴅᴏᴇʀs", show_alert=True
+                "Jhaat Barabar Dimaag Nhi Aur Owner Banenge", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(helpers.HELP_5, reply_markup=keyboard)
